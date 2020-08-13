255f0dd52a89461beb5cf6e59e0c77c352df8883
Josh Raiborde <joshsrai@yahoo.com>
2020-08-13 00:46:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,13 @@
 class Owner
+  
   attr_reader :name, :species
   
-  @@all= []
+  @@all = []
   
   def initialize(name)
     @name = name
-    @species = species
-    @@all << self 
+    @species = "human"
+    @@all << self
   end
   
   def say_species
@@ -14,19 +15,63 @@ class Owner
   end
   
   def self.all
-    @all 
+    @@all
   end
+  
+#  def self.owners
+#    Owner.all.count
+#  end
   
   def self.count
     @@all.count
   end
   
+  def self.reset_all
+    @@all.clear
+    #@@all = []
+  end
   
-  #def self.reset.all
-   # @@all.clear
-  #end
+  def cats
+    Cat.all.select {|c| c.owner == self}
+  end
   
-  def cats 
-    Cat.all.select 
+  def dogs
+    Dog.all.select {|d| d.owner == self}
+  end
+  
+  def buy_cat(name)
+    Cat.new(name,self)
+  end 
 
+  def buy_dog(name)
+    Dog.new(name,self)
+  end 
+  
+  def walk_dogs
+    dogs.each{|d| d.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.each{|c| c.mood = "happy"}
+  end
+  
+  def pets
+#    pets = []
+#    pets << dogs
+#    pets << cats
+#    pets.flatten
+    pets = dogs + cats
+  end
+  
+  def sell_pets
+    pets.each do |p|
+      p.mood = "nervous"
+      p.owner = nil
+    end
+  end
+  
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+    
 end
